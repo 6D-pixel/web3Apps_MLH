5af101106a730f8dfe58375cead1337838d1d416
6D-pixel <dinesh7rd@gmail.com>
2024-01-21 19:21:21 +0530

    made some icon changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,12 +22,12 @@ export const Navbar = () => {
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
+            src="money-bag.svg"
             className="h-8"
             alt="casino log"
           />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Casino
+            Mr.Beast
           </span>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
