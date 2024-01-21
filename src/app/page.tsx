24013996a6e1f00a8600e54b893a21bee0f4ab47
6D-pixel <dinesh7rd@gmail.com>
2024-01-21 19:58:38 +0530

    add btn and sending money logic removed hero.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,48 @@
-'use client'
+"use client";
 import { useContext } from "react";
-import { Heros } from "./Heros";
 import { AccountId, Client, PrivateKey } from "@hashgraph/sdk";
 import { GlobalAppContext } from "@/contexts/GlobalAppContext";
+import { sendHbar } from "@/lib/hederaServices";
 
 export default function Home() {
   const { metamaskAccountAddress } = useContext(GlobalAppContext);
 
-  if (!process.env.NEXT_PUBLIC_MY_ACCOUNT_ID || !process.env.NEXT_PUBLIC_MY_PRIVATE_KEY) {
+  if (
+    !process.env.NEXT_PUBLIC_MY_ACCOUNT_ID ||
+    !process.env.NEXT_PUBLIC_MY_PRIVATE_KEY
+  ) {
     throw new Error(
       "Environment variables MY_ACCOUNT_ID and MY_PRIVATE_KEY must be present"
     );
   }
 
   // create your client
-  const myAccountId = AccountId.fromString(process.env.NEXT_PUBLIC_MY_ACCOUNT_ID);
-  const myPrivateKey = PrivateKey.fromString(process.env.NEXT_PUBLIC_MY_PRIVATE_KEY);
+  const myAccountId = AccountId.fromString(
+    process.env.NEXT_PUBLIC_MY_ACCOUNT_ID
+  );
+  const myPrivateKey = PrivateKey.fromString(
+    process.env.NEXT_PUBLIC_MY_PRIVATE_KEY
+  );
 
   const client = Client.forTestnet();
   client.setOperator(myAccountId, myPrivateKey);
   return (
     <main className="">
-      <Heros />
+      <section className="bg-gray-900 text-white">
+        <div className="container mx-auto flex flex-col items-center justify-center h-screen">
+          <h1 className="text-5xl font-bold mb-4">GiveAway Page</h1>
+          <p className="text-xl font-light">get a Treat</p>
+        </div>
+        <button
+          type="button"
+          className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+          onClick={() => {
+            sendHbar(client, myAccountId, AccountId.fromEvmAddress(0, 0, metamaskAccountAddress), 7, myPrivateKey)
+          }}
+        >
+          Solid
+        </button>
+      </section>
     </main>
   );
 }
