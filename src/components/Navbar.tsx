07b79d4a8c5bba10e00f46917bd280c7ff072f4d
6D-pixel <dinesh7rd@gmail.com>
2024-01-21 10:20:32 +0530

    Made changes to Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,26 @@
 export const Navbar = () => {
   return (
-    <div className="flex justify-between items-center bg-slate-500">
-      <div className="m-4">LOGO</div>
-      <div className="m-2">
-        <button>connect</button>
+    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-8"
+            alt="casino log"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Flowbite
+          </span>
+        </a>
+        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <button
+            type="button"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Connect
+          </button>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
